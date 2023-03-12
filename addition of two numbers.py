6255555811abe9ff670a2031faa5c502a7e39860
Nivetha S <112693580+Nivetha2004@users.noreply.github.com>
2023-03-12 16:55:18 +0530

    Add files via upload

---
+++ addition of two numbers.py
@@ -0,0 +1,7 @@
+#This program adds two numbers
+num1 = int(input("Please enter Number1...:"))
+num2 = int(input("Please enter Number2...:"))
+#Add two numbers
+sum = num1+num2
+#Display the sum
+print('The sum{0}and {1}is...{2}'.format(num1,num2,sum))
